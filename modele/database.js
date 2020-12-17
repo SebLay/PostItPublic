bdd4882e5b13a7652f566ad5f869cdf3d4ad0aa0
SebLay <61143186+SebLay@users.noreply.github.com>
2020-12-17 18:46:08 +0100

    changement virgules

--- modele/database.js
+++ modele/database.js
@@ -2,11 +2,11 @@ require("dotenv").config();
 const process = require("process");
 const pg = require("pg");
 const Pool = pg.Pool;
-const user=process.env.DB_USER,
-const host=process.env.DB_HOST,
-const database= process.env.DB_DATABASE,
-const password=process.env.DB_PASSWORD,
-const port= process.env.DB_PORT,
+const user=process.env.DB_USER;
+const host=process.env.DB_HOST;
+const database= process.env.DB_DATABASE;
+const password=process.env.DB_PASSWORD;
+const port= process.env.DB_PORT;
 
 const connectionString = `postgresql://${user}:${password}@${host}:${port}/${database}?ssl=true`
 
